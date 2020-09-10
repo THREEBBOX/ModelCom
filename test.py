684cc91b6a47a02a62ea9f16a796994fc79559f9
wangxy <961167202@qq.com>
2020-09-10 19:35:12 +0800

    9-9 蒙特卡洛方法 9-10 遗传算法

--- test.py
+++ test.py
@@ -1,34 +1,39 @@
 import numpy as np
-import cvxpy as cp
 
-import cvxpy as cp
-import numpy as np
 
-c1 = np.array([1, 1, 3, 4, 2])
-c2 = np.array([-8, -2, -3, -1, -2])
-a = np.array([[1, 1, 1, 1, 1], [1, 2, 2, 1, 6], [2, 1, 6, 0, 0], [0, 0, 1, 1, 5]])
-b = np.array([400, 800, 200, 200])
-x = cp.Variable(5, integer=True)
-obj = cp.Minimize(c1 * x ** 2 + c2 * x)
-con = [0 <= x, x <= 99, a * x <= b]
-prob = cp.Problem(obj, con)
-prob.solve()
-print("最优值为:", prob.value)
-print("最优解为：\n", x.value)
+data = [9.6,18.3,29,47.2,71.1,119.1,174.6,
+        257.3,350.7,441.0,513.3,559.7,594.8,
+        629.4,640.8,651.1,655.7,659.6,661.8]
+data = np.array(data)
+time=np.arange(0,19)
 
-# c1 = np.array([1, 1, 3, 4, 2])
-# c2 = np.array([-8, -2, -3, -1, -2])
-# a = np.array([
-#     [1, 1, 1, 1, 1],
-#     [1, 2, 2, 1, 6],
-#     [2, 1, 6, 0, 0],
-#     [0, 0, 1, 1, 5]
-# ])
-# b = np.array([400, 800, 200, 200])
-# x = cp.Variable(5, integer=True)
-# obj = cp.Minimize(c1 * x ** 2 + c2 * x)
-# con = [x >= 0, x <= 1, a * x<=b]
-# prob = cp.Problem(obj,con)
-# prob.solve()
-# print(prob.value)
-# print(x.value)
+matrix1 = np.c_[data[:-1],-data[:-1]**2]
+matrix2 = np.array(data[1:]-data[:-1])
+print(matrix2)
+#%%
+result = np.linalg.pinv(matrix1).dot(matrix2)
+print(result)
+x0 = data[0]
+xn=0;
+rlist = [x0]
+r = result[0]
+N = result[0] / result[1]
+print(N)
+for i in range(1,19):
+    xn = x0+r*x0*(1-x0/N)
+    x0 = xn
+    rlist.append(xn)
+print(rlist)
+#%%
+import matplotlib.pyplot as plt
+import pylab as mpl
+plt.figure()
+mpl.rcParams['font.sans-serif'] = ['FangSong']  # 指定默认字体
+mpl.rcParams['axes.unicode_minus'] = False
+plt.scatter(time,data,color='r',marker='*')
+plt.plot(time,rlist,color='b')
+plt.title("拉屎")
+plt.xlabel("拉屎")
+plt.legend(("s","d"))
+
+plt.show()
